61b6464f9730f6a9582e57a8401160e5107fc610
EJ <100031970+EJARNADO11@users.noreply.github.com>
2022-02-19 22:27:17 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+         <title> My Home Page </title>
+    </head>
+    <body>
+    <h1> Welcome To My Website! I am EJ ARNADO!</h1>
+    <ul>
+       <li><a href="index.php">HOME</a></li>
+       <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+    </ul>
+ </body>
+</html>
+     
