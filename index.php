03c0374498e6dcb278c077a488ff0805ac4843cf
EJ <100031970+EJARNADO11@users.noreply.github.com>
2022-03-09 07:38:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <h1> Welcome To My Website! I am EJ ARNADO!</h1>
     <ul>
        <li><a href="index.php">HOME</a></li>
-       <li><a href="/CLE1MT/index.html">CLEMT1</a></li>
+       <li><a href="/CLE1MT/index.html">MIDTERM EXAM</a></li>
         <li><a href="/LT2/recipes.html">Learning Task 2</a></li>
     </ul>
  </body>
